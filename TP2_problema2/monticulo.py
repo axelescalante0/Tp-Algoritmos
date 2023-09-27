7f3dffc7a48b9461c4a916d711a8985444d99371
axelescalante0 <141271318+axelescalante0@users.noreply.github.com>
2023-09-27 11:23:32 -0300

    carpeta monticulo

---
+++ TP2_problema2/monticulo.py
@@ -0,0 +1,15 @@
+from pythoned.arboles.monticuloBinario import MonticuloBinario
+
+miMonticulo = MonticuloBinario()
+miMonticulo.insertar(5)
+miMonticulo.insertar(7)
+miMonticulo.insertar(3)
+miMonticulo.insertar(11)
+
+print(miMonticulo.eliminarMin())
+
+print(miMonticulo.eliminarMin())
+
+print(miMonticulo.eliminarMin())
+
+print(miMonticulo.eliminarMin())
