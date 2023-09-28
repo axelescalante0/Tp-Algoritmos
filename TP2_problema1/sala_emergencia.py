872236dfed62e25e3e081f072d307b886b492c2f
axelescalante0 <141271318+axelescalante0@users.noreply.github.com>
2023-09-28 14:51:47 -0300

    creo sala_emergencia.py

---
+++ TP2_problema1/sala_emergencia.py
@@ -0,0 +1,14 @@
+from monticulo import MonticuloBinario
+
+class SalaEmergencia:
+    def __init__(self):
+        self.monticulo = MonticuloBinario()
+
+    def ingresar_paciente(self, paciente):
+        self.monticulo.insertar(paciente)
+    
+    def atender_paciente(self):
+       return self.monticulo.eliminarMin()
+    
+    def total_pacientes(self):
+        return self.monticulo.tamanoActual()
